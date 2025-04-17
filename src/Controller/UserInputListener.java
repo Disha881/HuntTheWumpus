66fbaf6bf73cf6983245be8fa702d3e4802275cc
Disha881 <88449387+Disha881@users.noreply.github.com>
2025-04-17 12:38:27 -0700

    Delete src directory

--- src/Controller/UserInputListener.java
+++
@@ -1,8 +0,0 @@
-// Controller/UserInputListener.java
-package Controller;
-
-public interface UserInputListener {
-    void movePlayer(int targetCaveId);
-    void shootArrow(int targetCaveId);
-    void restartGame();
-}
